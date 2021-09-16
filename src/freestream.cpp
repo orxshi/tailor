ac4e32aef4e3bc69f48bce40053a371fbbd21062
orxshi <orxan.shibli@gmail.com>
2021-09-16 14:11:00 +0300

    >

--- src/freestream.cpp
+++ src/freestream.cpp
@@ -107,8 +107,8 @@ namespace Tailor
         
         po::options_description desc{sdesc};
         desc.add_options()
-            (copt(sdesc, "type"), po::value<std::string>()->required(), "")
-            //(copt(sdesc, "cnt_x"), po::value<double>(), "")
+            //(copt(sdesc, "type"), po::value<std::string>()->required(), "")
+            (copt(sdesc, "cnt_x"), po::value<double>(), "")
             //(copt(sdesc, "cnt_y"), po::value<double>(), "")
             //(copt(sdesc, "cnt_z"), po::value<double>(), "")
             //(copt(sdesc, "strength"), po::value<double>(), "")
@@ -129,8 +129,8 @@ namespace Tailor
         po::store(po::parse_config_file(settings_file, op, true), vm);
         po::notify(vm);
 
-        type = vm[sopt(sdesc, "type")].as<std::string>();
-        //cnt_x = vm[sopt(sdesc, "cnt_x")].as<double>();
+        //type = vm[sopt(sdesc, "type")].as<std::string>();
+        cnt_x = vm[sopt(sdesc, "cnt_x")].as<double>();
         //cnt_y = vm[sopt(sdesc, "cnt_y")].as<double>();
         //cnt_z = vm[sopt(sdesc, "cnt_z")].as<double>();
         //strength = vm[sopt(sdesc, "strength")].as<double>();
