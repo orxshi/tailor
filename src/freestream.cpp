637ef3bc7c6b2687edc761fe1286575c06b70c84
orxshi <orxan.shibli@gmail.com>
2021-09-16 13:06:42 +0300

    >

--- src/freestream.cpp
+++ src/freestream.cpp
@@ -100,15 +100,15 @@ namespace Tailor
         po::options_description desc{sdesc};
         desc.add_options()
             (opt(sdesc, "type"), po::value<std::string>()->required(), "")
-            ("cnt_x", po::value<double>(), "")
-            ("cnt_y", po::value<double>(), "")
-            ("cnt_z", po::value<double>(), "")
-            ("strength", po::value<double>(), "")
-            ("rho", po::value<double>(), "")
-            ("p", po::value<double>(), "")
-            ("u", po::value<double>(), "")
-            ("v", po::value<double>(), "")
-            ("w", po::value<double>(), "")
+            (opt(sdesc, "cnt_x"), po::value<double>(), "")
+            (opt(sdesc, "cnt_y"), po::value<double>(), "")
+            (opt(sdesc, "cnt_z"), po::value<double>(), "")
+            (opt(sdesc, "strength"), po::value<double>(), "")
+            (opt(sdesc, "rho"), po::value<double>(), "")
+            (opt(sdesc, "p"), po::value<double>(), "")
+            (opt(sdesc, "u"), po::value<double>(), "")
+            (opt(sdesc, "v"), po::value<double>(), "")
+            (opt(sdesc, "w"), po::value<double>(), "")
             ;
 
         op.add(desc);
@@ -121,16 +121,16 @@ namespace Tailor
         po::store(po::parse_config_file(settings_file, op, true), vm);
         po::notify(vm);
 
-        type = vm["type"].as<std::string>();
-        cnt_x = vm["cnt_x"].as<double>();
-        cnt_y = vm["cnt_y"].as<double>();
-        cnt_z = vm["cnt_z"].as<double>();
-        strength = vm["strength"].as<double>();
-        rho = vm["rho"].as<double>();
-        p = vm["p"].as<double>();
-        u = vm["u"].as<double>();
-        v = vm["v"].as<double>();
-        w = vm["w"].as<double>();
+        type = vm[opt(sdesc, "type")].as<std::string>();
+        cnt_x = vm[opt(sdesc, "cnt_x")].as<double>();
+        cnt_y = vm[opt(sdesc, "cnt_y")].as<double>();
+        cnt_z = vm[opt(sdesc, "cnt_z")].as<double>();
+        strength = vm[opt(sdesc, "strength")].as<double>();
+        rho = vm[opt(sdesc, "rho")].as<double>();
+        p = vm[opt(sdesc, "p")].as<double>();
+        u = vm[opt(sdesc, "u")].as<double>();
+        v = vm[opt(sdesc, "v")].as<double>();
+        w = vm[opt(sdesc, "w")].as<double>();
     }
 
     //void GaussianInit::read()
