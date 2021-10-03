6b0091d9254eb5dff5ba6ef516b8e26ea0c58a82
orxshi <orxan.shibli@gmail.com>
2021-10-03 12:44:01 +0300

    >

--- test/airfoil_moving_single_mesh/main.cpp
+++ test/airfoil_moving_single_mesh/main.cpp
@@ -5,32 +5,38 @@ std::vector<Tailor::AeroCoefPara> set_aero_para()
     Tailor::Freestream fs;
     fs.read();
 
+    Tailor::Component component;
+    component.read(Tailor::Tag(1));
+
     Tailor::AeroCoefPara aero_para;
     aero_para.p_ref = fs.p_;
     aero_para.rho_ref = fs.rho_;
     aero_para.u_ref = fs.u_;
+    aero_para.area_ref = 1.;
+    aero_para.moment_center = component.pivot_;
+    aero_para.moment_length = component.chord;
 
-    std::vector<Tailor::AeroCoefPara> aero_para_vec(1, aero_para);
+    std::vector<Tailor::AeroCoefPara> aero_para_vec(2, aero_para);
 
     return aero_para_vec;
 }
 
 void rotate(Tailor::Tailor& tailor)
 {
-    Tailor::Component compo;
-    compo.read(Tailor::Tag(1));
+    Tailor::Component component;
+    component.read(Tailor::Tag(1));
 
     auto solver = tailor.solver();
 
-    double reduced_freq = compo.reduced_freq;
-    double u = compo.u;
-    double v = compo.v;
-    double w = compo.w;
+    double reduced_freq = component.reduced_freq;
+    double u = component.u;
+    double v = component.v;
+    double w = component.w;
     Tailor::Vector3 U(u, v, w);
     double u_ref = U.len();
-    double chord = compo.chord;
-    double aoa_mean_deg = compo.aoa_mean_deg; 
-    double aoa_o_deg = compo.aoa_o_deg;
+    double chord = component.chord;
+    double aoa_mean_deg = component.aoa_mean_deg; 
+    double aoa_o_deg = component.aoa_o_deg;
     double aoa_mean = Tailor::deg_to_rad(aoa_mean_deg); 
     double aoa_o = Tailor::deg_to_rad(aoa_o_deg); 
 
@@ -38,10 +44,9 @@ void rotate(Tailor::Tailor& tailor)
 
     double aoa = aoa_mean + aoa_o * std::sin(om * solver->nsolve() * solver->dt());
 
-    int axis = compo.rotaxis_;
-    Tailor::Vector3 pivot(compo.pivot_(0), compo.pivot_(1), compo.pivot_(2));
+    int axis = component.rotaxis_;
 
-    tailor.rotate(Tailor::Tag(1), aoa, axis, pivot);
+    tailor.rotate(Tailor::Tag(1), aoa, axis, component.pivot_);
 }
 
 int main()
