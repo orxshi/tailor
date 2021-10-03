b2b24429c1b349a50e292545f09d38a81b2342e4
orxshi <orxan.shibli@gmail.com>
2021-10-03 15:19:45 +0300

    >

--- src/solver.cpp
+++ src/solver.cpp
@@ -1474,11 +1474,15 @@ namespace Tailor
 
             auto limit_coef = limiter.limit(mesh, mc, mc.gradient_);
 
+            std::cout << "grad(0): " << mc.gradient_[0](0) << " " << mc.gradient_[0](1) << " " << mc.gradient_[0](2) << " " << limit_coef[0] << std::endl;
+
             for (int i = 0; i < NVAR; ++i)
             {
                 mc.gradient_[i] *= limit_coef[i];
             }
         }
+
+        assert(false);
     }
 
     Vector5 Solver::non_linear_iteration()
