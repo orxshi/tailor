fb86b9bf7ec01c306dfe17427f83bd83155daba3
orxshi <orxan.shibli@gmail.com>
2021-10-04 16:41:59 +0300

    >

--- src/limiter.cpp
+++ src/limiter.cpp
@@ -46,9 +46,16 @@ namespace Tailor
     {
         // See page 3 of http://tetra.mech.ubc.ca/ANSLab/publications/michalak2008.pdf
 
-        auto foo = [&] (double x)
+        double K = 1.0;
+        double epsq = std::pow(K * std::pow(mc.poly().volume(), 1./3.), 3.);
+
+        auto foo = [&] (double ext, double df)
         {
-            return (std::pow(x, 2.) + 2. * x) / (std::pow(x, 2.) + x + 2.);
+            double dif_sq = ext * ext;
+            double df_sq = df * df;
+            double nom = (dif_sq + epsq) * df + 2. * df_sq * ext;
+            double denom = dif_sq + 2. * df_sq + df * ext + epsq;
+            return ((nom / denom) / df);
         };
 
         std::array<double, NVAR> phi;
@@ -90,15 +97,15 @@ namespace Tailor
 
             for (int k=0; k<NVAR; ++k)
             {
-                double urecon = mc.prim(k) + dot(grad[k], dis);  // unconstrained reconstructed value.
+                double df = dot(grad[k], dis);
 
-                if (urecon > 0.)
+                if (df > 0.)
                 {
-                    phi[k] = std::min(phi[k], foo(max_dif[k] / urecon));
+                    phi[k] = foo(max_dif[k], df);
                 }
-                else if (urecon < 0.)
+                else if (df < 0.)
                 {
-                    phi[k] = std::min(phi[k], foo(min_dif[k] / urecon));
+                    phi[k] = foo(min_dif[k], df);
                 }
                 else
                 {
@@ -109,6 +116,74 @@ namespace Tailor
 
         return phi;
     }
+
+    //std::array<double, NVAR> Limiter::venkatakrishnan(const Mesh& mesh, const MeshCell& mc, const std::array<Vector3, NVAR>& grad)
+    //{
+    //    // See page 3 of http://tetra.mech.ubc.ca/ANSLab/publications/michalak2008.pdf
+
+    //    auto foo = [&] (double x)
+    //    {
+    //        return (std::pow(x, 2.) + 2. * x) / (std::pow(x, 2.) + x + 2.);
+    //    };
+
+    //    std::array<double, NVAR> phi;
+    //    std::array<double, NVAR> max_dif;
+    //    std::array<double, NVAR> min_dif;
+
+    //    for (int k=0; k<NVAR; ++k)
+    //    {
+    //        max_dif[k] = TAILOR_BIG_NEG_NUM;
+    //        min_dif[k] = TAILOR_BIG_POS_NUM;
+    //        phi[k] = TAILOR_BIG_POS_NUM;
+    //    }
+
+    //    for (const MeshFace& face: mc.face())
+    //    {
+    //        const MeshCell* nei = opposing_nei(mesh, face, mc.tag());
+    //        assert(nei != nullptr);
+
+    //        for (int k=0; k<NVAR; ++k)
+    //        {
+    //            double dif = nei->prim(k) - mc.prim(k);
+
+    //            max_dif[k] = std::max(max_dif[k], dif);
+    //            min_dif[k] = std::min(min_dif[k], dif);
+    //        }
+    //    }
+
+    //    for (const MeshFace& face: mc.face())
+    //    {
+    //        const MeshCell* nei = opposing_nei(mesh, face, mc.tag());
+    //        assert(nei != nullptr);
+
+    //        auto dis = nei->poly().centroid() - mc.poly().centroid();
+
+    //        if (face.is_boundary())
+    //        {
+    //            dis *= 2.;
+    //        }
+
+    //        for (int k=0; k<NVAR; ++k)
+    //        {
+    //            double urecon = mc.prim(k) + dot(grad[k], dis);  // unconstrained reconstructed value.
+
+    //            if (urecon > 0.)
+    //            {
+    //                phi[k] = std::min(phi[k], foo(max_dif[k] / urecon));
+    //            }
+    //            else if (urecon < 0.)
+    //            {
+    //                phi[k] = std::min(phi[k], foo(min_dif[k] / urecon));
+    //            }
+    //            else
+    //            {
+    //                phi[k] = std::min(phi[k], 1.);
+    //            }
+    //        }
+    //    }
+
+    //    return phi;
+    //}
 
     std::array<double, NVAR> Limiter::barth_jespersen(const Mesh& mesh, const MeshCell& mc, const std::array<Vector3, NVAR>& grad)
     {
@@ -153,15 +228,15 @@ namespace Tailor
 
             for (int k=0; k<NVAR; ++k)
             {
-                double urecon = mc.prim(k) + dot(grad[k], dis);  // unconstrained reconstructed value.
+                double df = dot(grad[k], dis);
 
-                if (urecon > 0.)
+                if (df > 0.)
                 {
-                    phi[k] = std::min(phi[k], std::min(1., max_dif[k] / urecon));
+                    phi[k] = std::min(phi[k], std::min(1., max_dif[k] / df));
                 }
-                else if (urecon < 0.)
+                else if (df < 0.)
                 {
-                    phi[k] = std::min(phi[k], std::min(1., min_dif[k] / urecon));
+                    phi[k] = std::min(phi[k], std::min(1., min_dif[k] / df));
                 }
                 else
                 {
--- src/solver.cpp
+++ src/solver.cpp
@@ -937,18 +937,6 @@ namespace Tailor
                 {
                     mc.dQ_ = mc.runge_kutta_coef_[0] / 6. + mc.runge_kutta_coef_[1] / 3. + mc.runge_kutta_coef_[2] / 3. + mc.runge_kutta_coef_[3] / 6.;
                 }
-
-                //if (mc.tag()() == 452)
-                //{
-                //    //if (runge_kutta_stage == 3)
-                //    {
-                //        std::cout << "runge_kuuta_coef_[0](0): " << runge_kutta_coef_[0](0) << std::endl;
-                //        std::cout << "runge_kuuta_coef_[1](0): " << runge_kutta_coef_[1](0) << std::endl;
-                //        std::cout << "runge_kuuta_coef_[2](0): " << runge_kutta_coef_[2](0) << std::endl;
-                //        std::cout << "runge_kuuta_coef_[3](0): " << runge_kutta_coef_[3](0) << std::endl;
-                //        std::cout << "mc.dQ_(0): " << mc.dQ_(0) << std::endl;
-                //    }
-                //}
             }
         }
     }
