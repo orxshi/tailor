6ff99a50e745b238e01570f80d40fa5168ba8332
orxshi <orxan.shibli@gmail.com>
2021-10-04 21:43:17 +0300

    >

--- src/spc.cpp
+++ src/spc.cpp
@@ -164,10 +164,14 @@ namespace Tailor
             bool compute_force_coef = component.compute_force_coef;
             bool compute_moment_coef = component.compute_moment_coef;
 
+                std::cout << "ccccccccccccccccccccccccc" << std::endl;
+
             if (!compute_pres_coef && !compute_force_coef && !compute_moment_coef)
             {
                 continue;
             }
+
+                std::cout << "bbbbbbbbbbbbbbbbbbbbbbb" << std::endl;
 
             if (mesh != sp_.front().mesh().end())
             {
@@ -232,6 +236,7 @@ namespace Tailor
 
             if (compute_pres_coef)
             {
+                std::cout << "aaaaaaaaaaaaaaaaaa" << std::endl;
                 if (mesh != sp_.front().mesh().end())
                 {
                     std::string fn = "pres-coef-";
