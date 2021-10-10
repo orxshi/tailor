a61ab33b91cb96aa2bbe09c16d37b9438fceab7a
orxshi <orxan.shibli@gmail.com>
2021-10-10 17:32:05 +0300

    >

--- src/loadmap.cpp
+++ src/loadmap.cpp
@@ -679,10 +679,13 @@ namespace Tailor
         BinRMTag heaviest_bt;
         bool ire = resolution_is_enough(heaviest_bt, nmake_map);
 
+        std::cout << "Starting refining" << std::endl;
+
         if (dorefine)
         {
             while (!ire || nrefine < forced_n_refine_ || nrefine < min_forced_n_refine_)
             {
+                std::cout << "nrefine: " << nrefine << std::endl;
                 ++nrefine;
                 int new_rmtag;
                 int new_bt;
