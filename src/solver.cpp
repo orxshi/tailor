c0c576bf9fb674b30f4b1845c5985181a10d9216
orxshi <orxan.shibli@gmail.com>
2021-10-15 20:35:57 +0300

    <

--- src/solver.cpp
+++ src/solver.cpp
@@ -1637,7 +1637,7 @@ namespace Tailor
 
     void Solver::print_sub_solver_residual(int ntimestep, const Vector5& residual)
     {
-        if (!steady_ && !dual_ts_)
+        if (!steady_ && !implicit)
         {
             return;
         }
--- src/spc.cpp
+++ src/spc.cpp
@@ -233,7 +233,6 @@ namespace Tailor
             boost::mpi::all_reduce(*comm_, local.data(), 7, global.data(), std::plus<double>());
 
             double aoa = -1;
-            double rad_vel_z = -1;
                 if (comm_->rank() == 0)
                 {
                     //if (component.oscillation)
@@ -253,8 +252,6 @@ namespace Tailor
                         double om = 2. * reduced_freq * u_ref / chord; // rad/s
 
                         aoa = aoa_mean + aoa_o * std::sin(om * iter * dt);
-
-                        rad_vel_z = aoa_o * om * std::cos(om * iter * dt); // z-component of angular velocity.
                     }
                     //else if (component.rotation_)
                     //{
@@ -282,8 +279,6 @@ namespace Tailor
                     out << iter;
                     out << " ";
                     out << aoa;
-                    out << " ";
-                    out << -rad_vel_z;
                     out << " ";
                     out << global[0]; // cA
                     out << " "; 
