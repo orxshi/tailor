7760d1119b7211c31325a9a15959474c9f4baa81
orxshi <orxan.shibli@gmail.com>
2021-10-17 14:56:02 +0300

    <

--- src/meshface.cpp
+++ src/meshface.cpp
@@ -343,7 +343,7 @@ namespace Tailor
 
                     //vel = cross(rad_vel, project);
                     //if (btype_ == BouType::wall) // TODO just to test.
-                    if (btype_ == BouType::wall && btype_ == BouType::interior && btype_ == BouType::farfield) // TODO just to test.
+                    if (btype_ == BouType::wall && btype_ == BouType::interior && btype_ == BouType::farfield && btype_ == BouType::empty) // TODO just to test.
                     {
                         vel = cross(rad_vel, r);
                     }
